a934404f7bd0f3a767d28391f9ed75c4e4e69a47
Honey Hu <ghoneymail@gmail.com>
2018-06-10 23:06:57 +0800

    aheadbehind

--- graph.d.ts
+++ graph.d.ts
@@ -2,6 +2,9 @@ import { Repository } from './repository';
 import { Oid } from './oid';
 
 export class Graph {
-    static aheadBehind(repo: Repository, local: Oid, upstream: Oid): Promise<number>;
+    static aheadBehind(repo: Repository, local: Oid, upstream: Oid): Promise<{
+        ahead: number
+        behind: number
+    }>;
     static descendantOf(repo: Repository, commit: Oid, ancestor: Oid): Promise<number>;
 }
