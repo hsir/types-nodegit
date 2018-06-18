54601e4b60f52ff1a14696252bd5e4526d814d06
honeyhu <honeyhu@tencent.com>
2018-06-18 20:27:52 +0800

    add object type

--- object.d.ts
+++ object.d.ts
@@ -2,6 +2,21 @@ import { Oid } from './oid';
 import { Repository } from './repository';
 import { Buf } from './buf';
 
+export namespace Object {
+    export enum TYPE {
+        ANY = -2,
+        BAD = -1,
+        BLOB = 3,
+        COMMIT = 1,
+        EXT1 = 0,
+        EXT2 = 5,
+        OFS_DELTA = 6,
+        REF_DELTA = 7,
+        TAG = 4,
+        TREE = 2,
+    }
+}
+
 export class Object {
     static size(type: number): number;
     static lookup(repo: Repository, id: Oid, type: number): Promise<Object>;
